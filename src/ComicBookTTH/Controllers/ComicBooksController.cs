f0ecdc1c14c92ca13b656bb637a9e148921574ea
xoverkill <thhprincex@gmail.com>
2017-03-31 17:02:10 +0800

    Added ComicBooksController

---
+++ src/ComicBookTTH/Controllers/ComicBooksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComicBookTTH.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+    }
+}
