f9bf167cd864ea7f97ca0f2aa2f9ecb349f06817
xoverkill <thhprincex@gmail.com>
2017-03-31 17:20:49 +0800

    Made changes to Detail() method in ComicBookController

--- src/ComicBookTTH/Controllers/ComicBooksController.cs
+++ src/ComicBookTTH/Controllers/ComicBooksController.cs
@@ -8,8 +8,14 @@ namespace ComicBookTTH.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail() {
-            return "Hello";
+        public ActionResult Detail() {
+
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Friday) {
+                return Redirect("/");
+            }
+
+            return Content("Hello");
+            
         }
     }
 }
